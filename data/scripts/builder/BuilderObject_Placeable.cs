434fa98ad279558ba48e60d084e102840ac1639c
Calvin D. <cdevogel25@gmail.com>
2025-11-18 09:55:28 -0600

    added generic parent class for builder objects

---
+++ data/scripts/builder/BuilderObject_Placeable.cs
@@ -0,0 +1,189 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class BuilderObject_Placeable : Area2D
+{
+	// what do other objects need to know about a placeable object?
+	// is it snapped in place?
+	public bool IsSnapped = false;
+	// is it connected to the root object?
+	public bool IsConnectedToRoot = false;
+	// is it being dragged?
+	public bool IsBeingDragged = true;
+	// is it dropped?
+	public bool IsDropped = false;
+
+	private bool _isRoot = false;
+	private bool _isMouseOver = false;
+	private float _snapDistance = 32.0f;
+	public List<Marker2D> SnapPoints = new List<Marker2D>();
+	private Vector2 _snapPosition = Vector2.Zero;
+
+	public override void _Ready()
+	{
+		foreach (Node2D c in GetChildren())
+		{
+			if (c is Marker2D marker && marker.Name.ToString().StartsWith("Snap"))
+			{
+				SnapPoints.Add(marker);
+			}
+		}
+		Position = GetGlobalMousePosition();
+	}
+
+	/* Every placeable object should have:
+	*  - Snapping (TryAutoSnap)
+	*  - Input handling for drag/drop
+	*  - Root finding and connection logic
+	*  - Visual feedback for snapping and connection status
+	*  - Edge detection (if the tile is on the edge of the structure and another
+	*    tile is picked up, available snap points should be highlighted)
+	*  - overlap detection
+	*/
+	
+	// method skeletons
+	public override void _Input(InputEvent @event)
+	{
+		// left mouse will pick up a single tile (if not the root tile)
+		// right mouse will cancel placement of a currently-dragged tile
+		// right mouse will pick up a tile that is already placed and all its connected tiles
+		// right mouse will also pick up the root tile and connected tiles if any exist
+
+		// old code, please god replace this
+		if (!_isMouseOver) return;
+		if (@event is InputEventMouseButton mouseEvent)
+		{
+			if (IsBeingDragged && mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
+			{
+				if (TryAutoSnap())
+				{
+					Position = _snapPosition;
+				}
+				else
+				{
+					Position = GetGlobalMousePosition();
+				}
+				IsBeingDragged = false;
+				GD.Print(Name + " placed at: " + Position);
+			}
+			else if (IsBeingDragged && mouseEvent.ButtonIndex == MouseButton.Right && mouseEvent.Pressed)
+			{
+				QueueFree();
+				GD.Print(Name + " placement cancelled.");
+			}
+			else if (!IsBeingDragged && mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
+			{
+				IsBeingDragged = true;
+				_FollowMouse();
+			}
+		}
+
+		if(@event is InputEventMouseMotion && IsBeingDragged)
+		{
+			_FollowMouse();
+		}
+	}
+
+	public bool TryAutoSnap()
+	{
+		// you need new snapping logic, cuz what you have
+		// in Hull.cs is not good
+		var allTiles = GetTree().GetNodesInGroup("BuilderPlaceableObjects");
+
+		BuilderObject_Placeable nearestObject = null;
+		Marker2D nearestSnapFrom = null;
+		Marker2D nearestSnapTo = null;
+		List<Marker2D> snapToPoints;
+
+		float nearestDistance = float.MaxValue;
+
+		foreach (BuilderObject_Placeable tile in allTiles)
+		{
+			if (tile == this) continue;
+			snapToPoints = tile.SnapPoints;
+
+			GD.Print("Checking tile: " + tile.Name);
+
+			foreach (Marker2D snapFrom in SnapPoints)
+			{
+				foreach (Marker2D snapTo in snapToPoints)
+				{
+					float distance = snapFrom.GlobalPosition.DistanceTo(snapTo.GlobalPosition);
+
+					if (distance < nearestDistance && distance <= _snapDistance)
+					{
+						nearestDistance = distance;
+						nearestObject = tile;
+
+						nearestSnapFrom = snapFrom;
+						nearestSnapTo = snapTo;
+					}
+				}
+			}
+		}
+
+		if (nearestObject != null && nearestDistance <= _snapDistance)
+		{
+			// find best snap position
+			Vector2 offset = Position - nearestSnapFrom.GlobalPosition;
+			_snapPosition = nearestSnapTo.GlobalPosition + offset;
+			return true;
+
+			if (!_WouldOverlap(nearestObject))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+
+
+	private void _FindRoot()
+	{
+		// the root object will inherit from this class
+		if (_isRoot) return;
+		// logic to find and connect to root object
+	}
+
+	public void EdgeDetection()
+	{
+		// logic for edge detection and highlighting snap points
+		// check all snap points to see if anything is connected
+	}
+
+
+	private void _FollowMouse()
+	{
+		if (IsBeingDragged)
+		{
+			Position = GetGlobalMousePosition();
+		}
+	}
+
+	private bool _WouldOverlap(BuilderObject_Placeable nearestObject)
+	{
+		// logic to check for overlap with other objects
+		// there's gotta be a better way to do this using the built-in collision stuff
+
+		Vector2 thisSize = GetNode<CollisionShape2D>("ObjectCollisionShape").Shape.GetRect().Size;
+		var overlapRect = new Rect2(_snapPosition, thisSize * Scale);
+		Rect2 otherPosition = nearestObject.GetNode<CollisionShape2D>("ObjectCollisionShape").Shape.GetRect();
+		otherPosition = new Rect2(nearestObject.GlobalPosition, otherPosition.Size * Scale);
+
+		return overlapRect.Intersects(otherPosition);
+	}
+
+	// this is a hacky and bad way to do this but it has to work for now
+	private void _OnMouseEntered()
+	{
+		_isMouseOver = true;
+	}
+
+	private void _OnMouseExited()
+	{
+		_isMouseOver = false;
+	}
+}
